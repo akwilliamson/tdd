18459f69c923de7189892e2761d6d96e1b65d9e9
Aaron Williamson <wikea1@gmail.com>
2014-08-21 14:58:22 -0700

    Fix indentation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,27 +1,27 @@
 class Person < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :comapny_name, :email
+  attr_accessible :first_name, :last_name, :company_name, :email
   
   has_many :notes
 
   def self.merge(person_1, person_2)
-  	if person_1.email.include?("winner")
-  		person_1.notes << person_2.notes
-  		Person.find(person_2.id).delete
-  		return person_1
-  	else
-  		person_2.notes << person_1.notes
-  		Person.find(person_2.id).delete
-  		return person_2
-  	end
+    if person_1.email.include?("winner")
+      person_1.notes << person_2.notes
+      Person.find(person_2.id).delete
+      return person_1
+    else
+      person_2.notes << person_1.notes
+      Person.find(person_2.id).delete
+      return person_2
+    end
   end
 
   def duplicates?(person)
-  	unless person == nil
-  		if self[:first_name] == person[:first_name] && 
-  			 self[:last_name] == person[:last_name] && 
-  		 	 self[:email] == person[:email]
-  		 	 return true
-  		end
-  	end
+    unless person == nil
+      if self[:first_name] == person[:first_name] && 
+         self[:last_name] == person[:last_name] && 
+         self[:email] == person[:email]
+         return true
+      end
+    end
   end
 end
